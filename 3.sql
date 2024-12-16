ef2f4d9dc41311f27ed10c02417aaa830c75aaeb
Hendrawan O <hendrawanoktavianto786@gmail.com>
2024-12-16 21:12:26 +0700

    Revisi 3 SQL

--- 3.sql
+++ 3.sql
@@ -1,34 +1,57 @@
 1. Subquery: Tampilkan nama produk dan kategori untuk produk yang memiliki harga lebih tinggi daripada rata-rata harga semua produk.
+
+SELECT * FROM Produk
+
+SELECT NamaProduk, Kategori, Harga  FROM Produk p WHERE Harga > (
+SELECT AVG(Harga) FROM Produk p2 
+) 
+
 2. Conditional Expression: Tambahkan kolom Ketersediaan (Virtual Column) untuk menampilkan status produk:
 Tersedia jika stok lebih dari 10.
 Habis jika stok 0.
 Terbatas untuk stok antara 1 hingga 10.
+
+SELECT *,
+CASE
+	WHEN Stok > 10 THEN 'Tersedia'
+	WHEN Stok = 0 THEN 'Habis'
+	WHEN Stok Between 1 AND 10 THEN 'Terbatas'
+END
+as Ketersediaan
+FROM Produk p
+
 tableproduk
 
-SELECT NamaProduk,Kategori,Harga FROM Produk WHERE Harga < (SELECT AVG(Harga) FROM Produk)
-
-SELECT *,CASE
-WHEN Stok > 10 THEN 'Tersedia'
-WHEN Stok = 0 THEN 'Habis'
-ELSE 'Terbatas'
-END as Hasil1
-FROM Produk WHERE Stok >= 0
-
-
 3. Subquery dan Join: Tampilkan nama produk, kategori, jumlah terjual untuk semua transaksi.
-4. Analisis Conditional Expression: Hitung total pendapatan per kategori. 
+
+SELECT p.NamaProduk,p.Kategori,p2.JumlahTerjual FROM Produk p
+INNER JOIN Penjualan p2 
+ON p.ProdukID = p2.ProdukID
+
+4. Analisis Conditional Expression: Hitung total pendapatan per kategori.
+
+SELECT SUM(p.Harga * p.Stok) as Omzet, Kategori FROM Produk p 
+INNER JOIN Penjualan p2 
+ON p.ProdukID = p2.ProdukID
+GROUP BY p.Kategori 
+
 tableproduk&Penjualan
 
-SELECT Produk.NamaProduk, Produk.Kategori, Penjualan.JumlahTerjual FROM Produk
-INNER JOIN Penjualan ON Produk.ProdukId = Penjualan.ProdukID
-
-SELECT g.NamaProduk,g.Kategori, p.JumlahTerjual, g.Harga, (p.JumlahTerjual * g.Harga) as Profit FROM produk g
-INNER JOIN Penjualan p ON g.ProdukID = p.ProdukID
-
-
 5. CTE: Buat query untuk menghitung total stok per kategori menggunakan CTE.
+
+WITH CTE1 AS (
+	SELECT SUM(Stok) as TotalStok, Kategori FROM Produk GROUP BY Kategori
+)
+SELECT * FROM CTE1
+
 6. CTE dan Join: Gunakan CTE untuk menghitung penjualan per produk, lalu gabungkan dengan tabel produk untuk menampilkan nama produk, kategori.
 
+WITH CTE1 AS (
+	SELECT * FROM Penjualan p
+)
+SELECT JumlahTerjual,NamaProduk,Kategori FROM CTE1 p2
+INNER JOIN Produk p 
+ON p2.ProdukID = p.ProdukID
 
 
 
