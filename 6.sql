a2968665eea20046e64f904b09c518f1fae30e74
Hendrawan O <hendrawanoktavianto786@gmail.com>
2024-12-17 08:13:53 +0700

    Revisi 5 SQL

---
+++ 6.sql
@@ -0,0 +1,34 @@
+Soal 
+1.Pencarian Kamar yang Tersedia Tampilkan semua kamar yang saat ini tersedia untuk dipesan.
+
+SELECT * FROM Rooms r
+WHERE Status = 'Available'
+
+2.Laporan Pemesanan Per Pelanggan Tampilkan laporan daftar pemesanan untuk setiap pelanggan, termasuk nama pelanggan, jenis kamar, tanggal check-in, dan tanggal check-out.
+
+SELECT c.FullName,r.RoomType,b.CheckInDate,b.CheckOutDate FROM Bookings b
+INNER JOIN Customer c
+ON b.CustomerID = c.CustomerID 
+INNER JOIN Rooms r 
+ON r.RoomID = b.RoomID
+
+3.Validasi Ketersediaan Kamar Sebelum Pemesanan Tulis query untuk memeriksa apakah kamar tertentu tersedia pada rentang tanggal tertentu.
+
+DECLARE @roomid INT;
+DECLARE @chekin DATE;
+DECLARE @chekout DATE;
+
+SET @roomid = 1;
+SET @chekin='2024-12-19';
+SET @chekout='2024-12-19';
+
+IF EXISTS(
+SELECT * FROM Bookings b WHERE RoomID = @roomid AND (@chekin BETWEEN CheckInDate AND CheckOutDate) OR (@chekout BETWEEN CheckInDate AND CheckOutDate)
+)
+PRINT 'Sedang Terisi'
+ELSE
+PRINT 'Kosong'
+
+
+
+
