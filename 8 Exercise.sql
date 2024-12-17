5ec64fed16b12ec03dcad72f6031d11fe8522406
Hendrawan O <hendrawanoktavianto786@gmail.com>
2024-12-17 12:03:16 +0700

    Revisi 9 SQL

--- 8 Exercise.sql
+++ 8 Exercise.sql
@@ -1,10 +1,42 @@
 1.Dirty Read Buat transaksi yang menunjukkan data dibaca sebelum di-commit oleh transaksi lain.
 
+--Update Tanpa Commit
+BEGIN TRANSACTION;
+UPDATE Orders
+SET Status = 'Processing' 
+WHERE OrderID = 999;
+
+--Select Tanpa Commit
+SET TRANSACTION ISOLATION LEVEL
+READ UNCOMMITTED;
+SELECT * FROM Orders WHERE OrderID = 999;
+
+--Rollback
+ROLLBACK;
+
+SELECT * FROM Orders o WITH (NOLOCK) WHERE o.OrderID = 999 
+
 2. Non-repeatable Read Tunjukkan perbedaan hasil baca karena transaksi lain melakukan perubahan.
 
+--Membaca Data
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+BEGIN TRANSACTION;
+SELECT * FROM Orders WHERE OrderID = 999
+
+--Melakukan Perubahan
+BEGIN TRANSACTION;
+UPDATE Orders SET Status = 'Shipped' WHERE OrderID = 999
+COMMIT;
+
+--Membaca Data Ke 2
+SELECT * FROM Orders o WHERE OrderID = 999
+ROLLBACK;
+
+
 3. Phantom Read Demonstrasikan transaksi yang membaca hasil berbeda karena ada penyisipan data oleh transaksi lain.
+
+
 
 4. Serializable Tunjukkan isolasi penuh sehingga transaksi lain tidak dapat menyisipkan data pada rentang yang sedang diakses.
 
 5. Snapshot Tunjukkan bagaimana snapshot isolation memberikan konsistensi tanpa mengunci data.
-
--- 9 Exercise.sql
+++ 9 Exercise.sql
@@ -1,7 +1,27 @@
 Soal 1:
 Buat stored procedure bernama GetEmployeeDetails yang menerima parameter @DepartmentID dan @MinSalary. Stored procedure ini harus mengembalikan semua karyawan dari departemen tertentu yang gajinya lebih besar atau sama dengan @MinSalary.
 Jalankan stored procedure tersebut dengan parameter @DepartmentID = 5 dan @MinSalary = 5000
+TABLEEMPLOYEE
 
+SELECT * FROM Employees e 
+
+CREATE PROCEDURE GetEmployeeDetailsHendra
+@DepartmentID INT,
+@MinSalary DECIMAL
+AS
+BEGIN
+SELECT * FROM Employees e WHERE DepartmentID = @DepartmentID AND Salary <= @MinSalary
+END;
+
+ALTER PROCEDURE GetEmployeeDetailsHendra
+@DepartmentID INT,
+@MinSalary DECIMAL
+AS
+BEGIN
+SELECT * FROM Employees e WHERE DepartmentID = @DepartmentID AND Salary >= @MinSalary
+END;
+
+EXEC GetEmployeeDetailsHendra @DepartmentID = 5, @MinSalary = 5000;
 
 Soal 2:
 
@@ -9,6 +29,9 @@ Berikan hak akses SELECT ke tabel Employees kepada pengguna User_A.
 Berikan hak akses EXECUTE untuk stored procedure GetEmployeeDetails kepada pengguna User_B.
 Cabut hak akses SELECT dari pengguna User_A.
 
+GRANT SELECT ON Employees TO User_A;
+GRANT EXECUTE ON GetEmployeeDetailsHendra TO User_B;
+REVOKE SELECT ON Employees FROM User_A;
 
 
 Soal 3:
@@ -18,7 +41,15 @@ Buat non-clustered index pada kolom Salary.
 Tunjukkan bagaimana query berikut menggunakan indeks untuk mempercepat pencarian data berdasarkan gaji:
 SELECT EmployeeID, Name, Salary FROM Employees WHERE Salary > 8000;
 
+CREATE CLUSTERED INDEX
+IX_Employees_EmployeeID ON Employees(EmployeeID);
 
+CREATE NONCLUSTERED INDEX
+IX_Employees_Salary ON Employees(Salary);
+
+SELECT EmployeeID, Name, Salary FROM Employees WHERE EmployeeID = 5;
+
+SELECT * FROM Orders o 
 
 Soal 4:
 
@@ -27,3 +58,17 @@ Jalankan stored procedure tersebut untuk memperbarui gaji karyawan dengan Employ
 Tambahkan non-clustered index pada kolom JobTitle dan optimalkan query berikut menggunakan indeks:
 SELECT EmployeeID, Name FROM Employees WHERE JobTitle = 'Manager';
 
+CREATE PROCEDURE UpdateEmployeeSalaryHendra
+@EmployeeID INT,
+@NewSalary DECIMAL
+AS
+BEGIN
+UPDATE Employees e SET Salary = @NewSalary WHERE EmployeeID = @EmployeeID
+END;
+
+EXEC UpdateEmployeeSalaryHendra @EmployeeID = 100, @NewSalary = 7500;
+
+CREATE NONCLUSTERED INDEX
+IX_Employees_JobTitle ON Employees(JobTitle);
+
+SELECT EmployeeID, Name FROM Employees WHERE JobTitle = 'Manager';
